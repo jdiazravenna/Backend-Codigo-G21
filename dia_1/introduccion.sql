40281689f4fb7a0a93cda18839cee08c3927fcdf
Juan Diaz <jjdiazsoluciones@gmail.com>
2024-11-20 21:11:52 -0500

    añadiendo datos

---
+++ dia_1/introduccion.sql
@@ -0,0 +1,7 @@
+--Así se define un comentario en BD
+-- DDL Data Definition Langueja, es un sub lenguaje de sql para definir como se almacenaran los datos
+
+create database pruebas;
+
+--\cls -> limpiará la terminal
+-- \c pruebas -> conecta a la BD pruebas creada
